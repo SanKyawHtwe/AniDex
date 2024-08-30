db03b05b5ad99b43fc24a01aeb0745a716bef9d7
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2024-08-30 23:32:48 +0630

    Setup detekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
     alias(libs.plugins.firebase.perf)
+    alias(libs.plugins.detekt)
 }
 
 android {
