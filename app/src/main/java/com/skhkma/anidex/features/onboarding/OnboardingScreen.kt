83a7786745bcd27c29a9559b345670bb759906b2
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2024-08-30 23:54:09 +0630

    Added get started button

--- app/src/main/java/com/skhkma/anidex/features/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/skhkma/anidex/features/onboarding/OnboardingScreen.kt
@@ -2,15 +2,22 @@
 
 package com.skhkma.anidex.features.onboarding
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,47 +39,61 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
     val pagerState = rememberPagerState(pageCount = {
         PAGE_COUNT
     })
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.weight(0.8f),
-        ) { page ->
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    painter = painterResource(
-                        id = R.drawable.onboarding_image1
-                    ),
-                    modifier = Modifier
-                        .size(300.dp)
-                        .clip(shape = RoundedCornerShape(18.dp)),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "Onboarding Image",
-                )
-                Text(
-                    text = stringResource(R.string.onboarding_header),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Text(
-                    text = stringResource(R.string.onboarding_body),
-                    style = MaterialTheme.typography.bodyLarge
-                )
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.weight(0.8f),
+            ) { page ->
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(
+                            id = R.drawable.onboarding_image1
+                        ),
+                        modifier = Modifier
+                            .size(300.dp)
+                            .clip(shape = RoundedCornerShape(18.dp)),
+                        contentScale = ContentScale.Crop,
+                        contentDescription = "Onboarding Image",
+                    )
+                    Text(
+                        text = stringResource(R.string.onboarding_header),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = stringResource(R.string.onboarding_body),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+
+            Indicator(
+                modifier = Modifier.weight(0.2f),
+                count = PAGE_COUNT,
+                currentIndex = pagerState.currentPage
+            )
+        }
+        AnimatedVisibility(
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .align(Alignment.BottomCenter),
+            enter = fadeIn(),
+            exit = fadeOut(),
+            visible = pagerState.currentPage == PAGE_COUNT - 1
+        ) {
+            Button(onClick = { }) {
+                Text(text = "Get Started")
             }
         }
-        Indicator(
-            modifier = Modifier.weight(0.2f),
-            count = PAGE_COUNT,
-            currentIndex = pagerState.currentPage
-        )
     }
-
 }
 
 @Preview(showBackground = true)
