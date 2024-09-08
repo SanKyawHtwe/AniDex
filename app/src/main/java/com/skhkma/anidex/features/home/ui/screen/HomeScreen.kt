62efffbe88389d13c9371dc59eb9e72af5eecf9c
htetnayaung <htetnayaung.hna29@gmail.com>
2024-09-08 14:37:02 +0630

    Updated bottom navigation bar item selected

--- app/src/main/java/com/skhkma/anidex/features/home/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/skhkma/anidex/features/home/ui/screen/HomeScreen.kt
@@ -10,9 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,13 +26,14 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.compose.AniDexTheme
 import com.skhkma.anidex.R
 import kotlinx.serialization.Serializable
 
@@ -58,8 +64,6 @@ fun HomeScreen(
     onNavigateToManga: () -> Unit
 ) {
 
-    var selectedItem by remember { mutableIntStateOf(0) }
-//    val items = listOf("Anime", "Manga", "Favourite", "Profile")
 
     val items = listOf(
         Screen.Anime,
@@ -68,20 +72,34 @@ fun HomeScreen(
         Screen.Profile
     )
 
+    val icons = listOf(
+        Icons.Filled.Home,
+        Icons.Filled.Face,
+        Icons.Filled.Favorite,
+        Icons.Filled.Person
+    )
+
     val navController = rememberNavController()
     Scaffold(
         modifier = modifier.fillMaxSize(),
         bottomBar = {
 
             NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
+                var selectedState by remember { mutableIntStateOf(0) }
+
                 items.forEachIndexed { index, screen ->
                     NavigationBarItem(
-                        icon = { Icon(Icons.Filled.Face, contentDescription = null) },
+                        icon = { Icon(icons[index], contentDescription = null) },
                         label = { Text(stringResource(screen.resourceId)) },
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.primary,
+                            unselectedIconColor = Color.Gray,
+                            selectedTextColor = MaterialTheme.colorScheme.primary,
+                            unselectedTextColor = Color.Gray
+                        ),
+                        selected = index == selectedState,
                         onClick = {
+                            selectedState = index
                             navController.navigate(
                                 when (index) {
                                     0 -> AnimeRoute
@@ -158,12 +176,12 @@ fun HomeScreen(
 }
 
 
-//@Preview(showBackground = true)
-//@Composable
-//fun HomeScreenPreview() {
-//    AniDexTheme {
-//        HomeScreen(
-//            onNavigateToManga = {}
-//        )
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    AniDexTheme {
+        HomeScreen(
+            onNavigateToManga = {}
+        )
+    }
+}
