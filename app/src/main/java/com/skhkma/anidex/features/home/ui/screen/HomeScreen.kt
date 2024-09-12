57d24c43062b617a3d4ce297721cced4795076ca
sankyawhtwe <sankyawhtwe.personal@gmail.com>
2024-09-13 00:35:16 +0630

    Collapsing tool bar in profile screen

--- app/src/main/java/com/skhkma/anidex/features/home/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/skhkma/anidex/features/home/ui/screen/HomeScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -19,12 +23,17 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -37,6 +46,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.skhkma.anidex.anime.ui.AnimeRoute
 import com.skhkma.anidex.anime.ui.animeScreen
+import com.skhkma.anidex.designsystem.R
 import com.skhkma.anidex.designsystem.theme.AniDexTheme
 import com.skhkma.anidex.profile.ui.ProfileRoute
 import com.skhkma.anidex.profile.ui.profileScreen
@@ -72,6 +82,7 @@ private val topLevelRoutes = listOf(
     TopLevelRoute("Profile", ProfileRoute, Icons.Filled.Person)
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -79,13 +90,32 @@ private fun HomeScreen(
     onNavigateToAuthLanding: () -> Unit
 ) {
     val navController = rememberNavController()
-
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     Scaffold(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+           if( currentDestination?.hierarchy?.any {
+                   it.hasRoute(topLevelRoutes.last().route::class) } == true){
+               CenterAlignedTopAppBar(
+                   modifier = Modifier,
+                   title = {
+                       Image(
+                           modifier = Modifier.fillMaxWidth(),
+                           painter = painterResource(R.drawable.place_holder_image),
+                           contentDescription = null,
+                           contentScale = ContentScale.FillWidth
+                       )
+                   },
+                   scrollBehavior = scrollBehavior
+               )
+           }
+        },
         bottomBar = {
             NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
+
                 topLevelRoutes.forEach { topLevelRoute ->
                     NavigationBarItem(
                         icon = {
--- feature/profile/src/main/java/com/skhkma/anidex/profile/ui/ProfileScreen.kt
+++ feature/profile/src/main/java/com/skhkma/anidex/profile/ui/ProfileScreen.kt
@@ -1,21 +1,40 @@
 package com.skhkma.anidex.profile.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.skhkma.anidex.designsystem.theme.AniDexTheme
 import kotlinx.serialization.Serializable
+import com.skhkma.anidex.designsystem.R as designR
 
 @Serializable
 data object ProfileRoute
 
 fun NavController.navigateToProfileScreen() {
-    navigate(com.skhkma.anidex.profile.ui.ProfileRoute)
+    navigate(ProfileRoute)
 }
 
 fun NavGraphBuilder.profileScreen(
@@ -33,19 +52,60 @@ private fun ProfileScreen(
     modifier: Modifier = Modifier,
     onNavigateToAuthLanding: () -> Unit
 ) {
-    Button(
-        modifier = modifier,
-        onClick = {
-
-            onNavigateToAuthLanding()
-        }
+    val scrollState = rememberScrollState(initial = 30)
+    Column(
+        modifier = modifier
+            .height(3000.dp)
+            .verticalScroll(state = scrollState)
     ) {
-        Text("Logout")
+        Row(
+            modifier = Modifier
+                .height(80.dp)
+                .padding(horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(80.dp),
+                painter = painterResource(
+                    designR.drawable.user_profile,
+                ),
+                contentDescription = null,
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("User Name")
+                OutlinedButton(
+                    modifier = Modifier
+                        .heightIn(min = 24.dp),
+                    onClick = { },
+                    contentPadding = PaddingValues(
+                        vertical = 1.dp, horizontal = 16.dp
+                    )
+                ) {
+                    Text("Edit")
+                }
+            }
+        }
+        Button(
+            modifier = modifier,
+            onClick = {
+                onNavigateToAuthLanding()
+            }
+        ) {
+            Text("Logout")
+        }
     }
+
 }
 
 
-@Preview
+@Preview(showBackground = true, fontScale = 1.0f)
 @Composable
 private fun ProfileScreenPreview() {
     AniDexTheme {
